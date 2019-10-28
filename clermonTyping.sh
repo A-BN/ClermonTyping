6a1c0ac0b642f79402c0bf4e912cf173902832ff
Benedicte-C <benedicte.condamine@gmail.com>
2019-10-28 17:08:58 +0100

    Add option summary, v, fastafile, minimal and correction of one profile

--- clermonTyping.sh
+++ clermonTyping.sh
@@ -10,25 +10,25 @@
 # 4] Launch in silicco PCR for getting phylogroup
 # 5] Reportings tools
 #
-# Current version : 1.4.1 (Jun. 2019)
-version="Clermont Typing  Current version : 1.4.1 (Jun. 2019)"
+# Current version : 1.4.2 (Jun. 2019)
+version="Clermont Typing  Current version : 1.4.2 (Oct. 2019)"
 
 # Contact: antoine.bridier-nahmias@inserm.fr
 
-MY_PATH="`dirname \"$0\"`"
+MY_PATH=$(dirname "$0")
 #Default threshold = 0 (disabled)
 THRESHOLD=0
 #Default name = date
 DATE=$( date "+%F_%H%M%S")
 NAME=analysis_$DATE
-#Global variables
-THREADS=4
 #BLAST settings
 PRIMERS="${MY_PATH}/data/primers.fasta"
 PERC_IDENTITY=90
 BLAST_TASK='blastn'
 #MASH settings
 DB_MASH="${MY_PATH}/data/mash/mash_reference.msh"
+#Flag for minimal
+MINIMAL=0
 
 function usage(){
 	printf "Script usage :\n"
@@ -36,22 +36,25 @@ function usage(){
 	printf "\t-v					: print the version and exit\n"
 	printf "\t--fasta					: fasta contigs file(s). If multiple files, they must be separated by an arobase (@) value\n"
 	printf "\t--name					: name for this analysis (optional)\n"
-	printf "\t--threshold				: Option for ClermontTyping, do not use contigs under this size (optional)\n"
+	printf "\t--threshold				: option for ClermontTyping, do not use contigs under this size (optional)\n"
+	printf "\t--minimal				: output a minimal set of files (optional)\n"
+	printf "\t--fastafile				: file with path of fasta contig file.  One file by line (optional)\n"
+	printf "\t--summary				: file with path of *_phylogroups.txt. One file by line (optional)\n"
 }
 
 function mash_analysis(){
-	echo "============== Running mash ================"
-	${MY_PATH}/bin/mash screen -w $DB_MASH $FASTA >$WORKING_DIR/${FASTA_NAME}_mash_screen.tab
+	echo "===== Running mash ====="
+	"${MY_PATH}/bin/mash" screen -w "$DB_MASH" "$FASTA" > "$WORKING_DIR/${FASTA_NAME}_mash_screen.tab"
 }
 
 function blast_analysis(){
-	echo "============== Making blast db ================"
+	echo "===== Making blast db ====="
 	echo "makeblastdb -in $FASTA -input_type fasta -out $WORKING_DIR/db/$NAME -dbtype nucl"
-	makeblastdb -in $FASTA -input_type fasta -out $WORKING_DIR/db/$FASTA_NAME -dbtype nucl
+	makeblastdb -in "$FASTA" -input_type fasta -out "$WORKING_DIR/db/$FASTA_NAME" -dbtype nucl
 	if [ $? -eq 0 ]
 	then
-        echo "============== Running blast =================="
-        blastn -query $PRIMERS -perc_identity $PERC_IDENTITY -task $BLAST_TASK -word_size 6 -outfmt 5 -db $WORKING_DIR/db/$FASTA_NAME -out $WORKING_DIR/$FASTA_NAME.xml
+        echo "===== Running blast ====="
+        blastn -query "$PRIMERS" -perc_identity "$PERC_IDENTITY" -task "$BLAST_TASK" -word_size 6 -outfmt 5 -db "$WORKING_DIR/db/$FASTA_NAME" -out "$WORKING_DIR/$FASTA_NAME.xml"
         error=0
 	else
         echo "Error detected! Stopping pipeline..."
@@ -74,8 +77,8 @@ function report_calling(){
 	shift
 	out_dir=$1
 
-	modif_script=${out_dir}/${namus}.R
-	cp ${rscript} ${modif_script}
+	modif_script="${out_dir}/${namus}.R"
+	cp "${rscript}" "${modif_script}"
 
 	sed -i "s:TARTAMPION:$clermont_out:g" "${modif_script}"
 
@@ -92,79 +95,161 @@ fi
 while [[ $# -gt 0 ]]
 do
     case "$1" in
-	-v)
-	echo $version
-	usage
-	exit 0
-	;;
-        -h)
+    -v)
+        echo "$version"
         usage
         exit 0
         ;;
-        --fasta) 
-		FASTAS="$2";
+    -h)
+        usage
+        exit 0
+        ;;
+    --fastafile) 
+        FASTA_FILE="$2";
+        shift
+        ;;    
+    --fasta) 
+        FASTAS="$2";
         shift
         ;;
-        --name) 
-		NAME="$2";
+    --name) 
+        NAME="$2";
         shift
         ;;
-        --threshold) 
-		THRESHOLD="$2";
+    --minimal)
+        MINIMAL=1;
+        ;;	
+    --threshold) 
+        THRESHOLD="$2";
         shift
         ;;
-        --) shift; break;;
+    --summary) 
+        SUMMARY="$2";
+        shift
+        ;;    
+
+    --) shift; break;;
     esac
     shift
 done
 
-if [ -z $FASTAS ]
-then
-	echo "Missing the contigs file. Option --fasta"
-	usage
-	exit 1
-fi
 
-echo "You asked for a Clermont typing analysis named $NAME of phylogroups on $FASTAS with a threshold under $THRESHOLD."
+if [ -z "$FASTAS" ] && [ -z "$FASTA_FILE" ] && [ -z "$SUMMARY" ] 
+then
+    echo "Missing the contigs file. Option --fasta or --fastafile"
+    usage
+    exit 1
+elif [ -n "$FASTAS" ] && [ -n "$FASTA_FILE" ]
+then
+    echo "Too much parameters. Option --fasta or --fastafile"
+    usage  
+    exit 1
+elif [ -n "$SUMMARY" ] 
+then
+    NAME='Summary'
+    echo "You asked for a Clermont typing analysis named $NAME of phylogroups."
+    if  [ -n "$FASTA_FILE" ] || [ -n "$FASTAS" ] 
+    then
+        echo "Too much parameters. Option --fasta or --fastafile, or --summary"
+        usage
+        exit 1
+    fi
+
+else
+    echo "You asked for a Clermont typing analysis named $NAME of phylogroups on $FASTAS with a threshold under $THRESHOLD."
+fi
 
 if [ ! -d $NAME ]
 then
 	mkdir $NAME
 fi
-CURRENT_DIR=`pwd`
+CURRENT_DIR=$(pwd)
 WORKING_DIR=$CURRENT_DIR/$NAME
-#Analysis of each fasta file
-IFS='@' read -ra ARRAY_FASTA <<< "$FASTAS"
-for FASTA in "${ARRAY_FASTA[@]}"; do
-	if [ -f $FASTA ] && [ ! -z $FASTA ]
-	then
-		#Rename file
-		BASE_NAME_FASTA=`basename $FASTA`
-		FASTA_NAME=${BASE_NAME_FASTA%£*}
-		echo "Analysis of ${FASTA_NAME}"
-		cp $FASTA $WORKING_DIR/${FASTA_NAME}
-		FASTA=$WORKING_DIR/$FASTA_NAME
-		##### Step 1: MASH analysis #####
-		# Generate ${FASTA_NAME}_mash_screen.tab
-		mash_analysis
-		##### Step 2: Blast #############
-		# Generate ${FASTA_NAME}.xml
-		blast_analysis
-        if [ $error -gt 0 ]
+
+
+if [ -z "$SUMMARY" ]
+then
+    declare -a LIST_FILES
+    if [ -z "$FASTA_FILE" ]
+    then
+        IFS='@' read -ra ARRAY_FASTA <<< "$FASTAS"
+        LIST_FILES="${ARRAY_FASTA[@]}"
+    else    
+        while IFS=$'\n' read -r line 
+        do 
+            sample=${line%\\n}  
+            LIST_FILES+=("${sample}")
+        done < "${FASTA_FILE}"
+    fi
+
+    #Analysis of each fasta file
+    for FASTA in "${LIST_FILES[@]}"
+    do
+        if [ -f "$FASTA" ] && [ -n "$FASTA" ]
         then
-            echo "$FASTA_NAME				\"NA\"	${FASTA_NAME}_mash_screen.tab" >> $WORKING_DIR/${NAME}_phylogroups.txt
+            #Rename file
+            BASE_NAME_FASTA=$(basename "$FASTA")
+            FASTA_NAME=${BASE_NAME_FASTA%£*}
+            echo "============== Analysis of ${FASTA_NAME} =================="
+            cp "$FASTA" "$WORKING_DIR/${FASTA_NAME}"
+            FASTA="$WORKING_DIR/$FASTA_NAME"
+            
+            ##### Step 1: MASH analysis #####
+            # Generate ${FASTA_NAME}_mash_screen.tab
+            mash_analysis
+            
+            ##### Step 2: Blast #############
+            # Generate ${FASTA_NAME}.xml
+            blast_analysis
+            if [ $error -gt 0 ]
+            then
+                printf "%s\t\t\t\tNA\t%s__mash_screen.tab\n" "$FASTA_NAME" "$FASTA_NAME" >> "$WORKING_DIR/${NAME}_phylogroups.txt"
+            else
+                ##### Step 3: ClermonTyping #####
+                echo "====== ClermonTyping ====="
+                results=$("${MY_PATH}/bin/clermont.py" -x "${WORKING_DIR}/${FASTA_NAME}.xml" -s "$THRESHOLD")
+                printf "%s\t%s\t%s_mash_screen.tab\n" "$FASTA_NAME" "$results" "$FASTA_NAME" >> "$WORKING_DIR/${NAME}_phylogroups.txt"
+            fi
         else
-            ##### Step 3: ClermonTyping #####
-            echo "============== ClermonTyping =================="
-            results=`${MY_PATH}/bin/clermont.py -x ${WORKING_DIR}/${FASTA_NAME}.xml -s $THRESHOLD`
-            echo "$FASTA_NAME	$results	${FASTA_NAME}_mash_screen.tab" >> $WORKING_DIR/${NAME}_phylogroups.txt
+            echo "$FASTA doesn't exists"
         fi
-	else
-		echo "$FASTA doesn't exists"
-	fi
-done
+    done
+else
+    while IFS=$'\n' read -r line 
+    do 
+        IFS='/' read -ra ARRAY_PATH <<< "$line"
+        DIR="${ARRAY_PATH[0]}"
+    
+        IFS=$'\n'
+        for line2 in $(cat $line)
+        do
+            IFS=$'\t'
+            i=0
+            for line3 in $line2
+            do
+                if (( i < 5 ))
+                then
+                    printf "%s\t" "$line3" >> "${WORKING_DIR}/${NAME}_phylogroups.txt"
+                else
+                    printf "../%s/%s" "$DIR" "$line3" >> "${WORKING_DIR}/${NAME}_phylogroups.txt"
+                fi
+                ((i++))
+            done
+        done
+        printf "\n" >> "${WORKING_DIR}/${NAME}_phylogroups.txt"
+    done < "${SUMMARY}"
+fi
+
+
 ##### Step 4: Reporting #########
-report_calling "${MY_PATH}/bin/clermontReport.R" $WORKING_DIR/${NAME}_phylogroups.txt $NAME $WORKING_DIR
+if  [ ${MINIMAL} -eq 0 ] 
+then
+    report_calling "${MY_PATH}/bin/clermontReport.R" "$WORKING_DIR/${NAME}_phylogroups.txt" "$NAME" "$WORKING_DIR"
+fi
 
-
+echo "============== End =================="
 exit 0
+
+
+
+
